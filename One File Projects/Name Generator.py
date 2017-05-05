cc1126250db59147406afa8c86d161b732fe784e
Janbuller <lasse@thegamersofpro.com>
2017-05-05 09:42:04 +0200

    Lists to Tuples and added Jones to middleNames

--- One File Projects/Name Generator.py
+++ One File Projects/Name Generator.py
@@ -14,9 +14,9 @@ while tryNumber == 0:
         os.system('cls')
 amountDone = 1
 os.system('cls')
-firstNames = ["Adam", "Bob", "Tom", "Ellis", "Monet", "Franklyn", "Gwen", "Peter", "Neil", "Leonard", "Howard", "Brian","Penny", "Matthew", "Joe", "Albert", "Jane", "Gordon", "Jack", "Lois", "Kyle", "Kenny", "Steve", "Olivia","Janelle", "Malissa", "Hailey", "Katie", "James", "John", "Robert", "Micheal", "William", "Charles", "David"]
-lastNames = ["Short", "Harmon", "Jacobson", "Antonyson", "Bullard", "Washington", "Armstrong", "Tash", "Durand","Colbert", "Matthews", "Santoro", "Kennedy", "Swanson", "Smith", "Johnson", "Jones", "Clark", "Jackson","Robinson", "Miller", "Scottson", "Nelson", "Cooper", "Parker", "Adams", "Copeland", "Davenport", "Bruce", "Perry", "Fisher", "Russell"]
-middleNames = ["James", "Lee", "Micheal", "Joseph", "Alexander", "David", "William", "Andrew", "Matthew", "Robert", "Marie", "Ann", "Rose", "Mae", "Jane", "Thomas", "Nicole", "Lynn", "Grace", "Faith", "Elizabeth", "Renee", "Allen", "Anthony"]
+firstNames = ("Adam", "Bob", "Tom", "Ellis", "Monet", "Franklyn", "Gwen", "Peter", "Neil", "Leonard", "Howard", "Brian","Penny", "Matthew", "Joe", "Albert", "Jane", "Gordon", "Jack", "Lois", "Kyle", "Kenny", "Steve", "Olivia","Janelle", "Malissa", "Hailey", "Katie", "James", "John", "Robert", "Micheal", "William", "Charles", "David")
+lastNames = ("Short", "Harmon", "Jacobson", "Antonyson", "Bullard", "Washington", "Armstrong", "Tash", "Durand","Colbert", "Matthews", "Santoro", "Kennedy", "Swanson", "Smith", "Johnson", "Jones", "Clark", "Jackson","Robinson", "Miller", "Scottson", "Nelson", "Cooper", "Parker", "Adams", "Copeland", "Davenport", "Bruce", "Perry", "Fisher", "Russell")
+middleNames = ("James", "Lee", "Micheal", "Joseph", "Alexander", "David", "William", "Andrew", "Matthew", "Robert", "Marie", "Ann", "Rose", "Mae", "Jane", "Thomas", "Nicole", "Lynn", "Grace", "Faith", "Elizabeth", "Renee", "Allen", "Anthony", "Jones")
 while amountDone <= amount:
     num1 = randint(1, len(firstNames) - 1)
     num2 = randint(1, len(lastNames) - 1)
