8e3723f589a587c68426dc7acdda7a91d54ff540
Janbuller <lasse@thegamersofpro.com>
2017-05-05 10:30:36 +0200

    Added Addition Calculator

---
+++ One File Projects/Addition Calculator.py
@@ -0,0 +1,22 @@
+from os import system
+from time import sleep as slp
+print("Addition Calculator")
+numList = []
+result = 0
+whileLoop1 = 0
+while whileLoop1 == 0:
+    numList.append(int(input("Please type a number:")))
+    system('cls')
+    while True:
+        nextNumber = input("Do you want to type another number:")
+        if nextNumber == "yes" or nextNumber == "Yes" or nextNumber == "YES" or nextNumber == "yEs" or nextNumber == "yeS" or nextNumber == "yES" or nextNumber == "YeS":
+            break
+        elif nextNumber == "no" or nextNumber == "No" or nextNumber == "nO" or nextNumber == "NO":
+            whileLoop1 = 1
+            for i in numList:
+                result += i
+            print(result)
+        else:
+            print("Please only type Yes or No")
+            slp(2.5)
+            system('cls')
