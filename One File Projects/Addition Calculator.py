03f546425611f1975396bc908c59821c9feab0b2
Janbuller <lasse@thegamersofpro.com>
2017-05-05 10:53:58 +0200

    For Loop to Sum

--- One File Projects/Addition Calculator.py
+++ One File Projects/Addition Calculator.py
@@ -2,7 +2,6 @@ from os import system
 from time import sleep as slp
 print("Addition Calculator")
 numList = []
-result = 0
 whileLoop1 = 0
 while whileLoop1 == 0:
     numList.append(int(input("Please type a number:")))
@@ -13,9 +12,7 @@ while whileLoop1 == 0:
             break
         elif nextNumber == "no" or nextNumber == "No" or nextNumber == "nO" or nextNumber == "NO":
             whileLoop1 = 1
-            for i in numList:
-                result += i
-            print(result)
+            print(sum(numList))
         else:
             print("Please only type Yes or No")
             slp(2.5)
