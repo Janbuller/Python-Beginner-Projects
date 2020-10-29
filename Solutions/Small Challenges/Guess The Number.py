7a2da97135da0faba96c14d622e3a465d9e9ceda
Lasse Lykke Jensen <lasselj@yandex.com>
2020-10-29 13:47:38 +0100

    Slight grammar changes
    Fixed some grammar and added a variable, containing the initial number of lives.

--- Solutions/Small Challenges/Guess The Number.py
+++ Solutions/Small Challenges/Guess The Number.py
@@ -1,8 +1,10 @@
 import random
 
+starting_lives = 9
+
 print("Guessing Game")
-print("You have only 9 lives")
-print("You have to guess only from 1 to 50")
+print( f"You only have {starting_lives} lives")
+print("You have to guess a number between 1 to 50")
 
 no_of_lives = 0
 answer = random.randint(1, 50)
@@ -10,12 +12,12 @@ answer = random.randint(1, 50)
 while no_of_lives < 9:
     input_number = int(input("Guess the number: "))
     if input_number < answer:
-        print("Oops!, you guessed too less")
-        print(f"You have only {9 - no_of_lives} number of lives")
+        print("Oops!, you guessed too low")
+        print(f"You have only {starting_lives - no_of_lives} lives left")
     elif input_number > answer:
         print("Oops!, you guessed too high")
-        print ( f"You have only {9 - no_of_lives} number of lives" )
+        print ( f"You have only {starting_lives - no_of_lives} lives left" )
     else:
-        print(f"Congratulations!, you guessed the right number in {no_of_lives} number of lives")
+        print(f"Congratulations!\nYou guessed the right number in {no_of_lives} tries!")
         break
     no_of_lives += 1
